1c60c5269fa011a90444086310df008918a2232f
Rolf Stirnimann <rolf.stirnimann@alumni.esade.edu>
2020-05-26 10:33:00 +0200

    Update Labelling.R

--- 4_Politics/python3_script/data/Labelling.R
+++ 4_Politics/python3_script/data/Labelling.R
@@ -1,15 +1,15 @@
 library(data.table)
 #install.packages('bit64')
 
-library(sjmisc)
-library(dplyr)
+#library(sjmisc)
+#library(dplyr)
 
 #install.packages('devtools')
-devtools::install_github("favstats/AllSideR")
-allsides = AllSideR::allsides_data
+#devtools::install_github("favstats/AllSideR")
+#allsides = AllSideR::allsides_data
 
 #load dataset
-df = fread("~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/20200501-200819_joe_biden.csv")
+df = fread("~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/20200504-193926_joe_biden.csv")
 
 #create relevant column equals 1 if it contains keywords
 #115 videos contain keywords in title or description columns
@@ -27,22 +27,32 @@ sum(ifelse(grepl('biden|trump|democrat|republican|politics|president', df$descri
 #Look for fake news
 df2 = df[relevant==1, .(title, description, channel)]
 
-#huckabee is fake: The DEEP STATE vs General Flynn
-df[, .N, by='channel'][order(-N)][1:10]
+#write csv with channels and genre
+channels = df[, .N, by='channel'][order(-N)]
+channels_genre = df[, .N, by=c('channel','genre')]
+write.csv(channels_genre, "~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/channels/20200504-193926_joe_biden_channels.csv", row.names=FALSE)
+
+#write csv with genre
 genre = df[, .N, by='genre'][order(-N)]
+write.csv(genre, "~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/genre/20200504-193926_joe_biden_genre.csv", row.names=FALSE)
 
-write.csv(genre, "~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/genre/20200501-200819_joe_biden.csv")
+genre_relevant = fread("~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/genre/20200504-193926_joe_biden_genre.csv")
 
-channel_unique = data.table()
-channel_unique$channel = unique(df$channel)
+df2 = merge(df[, c('title','description','genre','channel','id')], 
+            genre_relevant, 
+            by='genre')
 
-#create new df by merging df2 with allsides
-#https://www.allsides.com/media-bias/media-bias-ratings
-channel_unique2 = merge(channel_unique, 
-            allsides[, c('news_source', 'rating', 'rating_num')], 
-            by.x='channel',
-            by.y='news_source', 
-            all.x=TRUE)
+#percentage of videos that are relevant
+mean(df2$relevant)
+
+#create table with only relevant videos
+relevant = df2[relevant == 1]
+
+#top25 channels weight
+sum(channels[1:25]$N) / sum(channels$N)
+
+#top25 channels - average number of videos per channel
+sum(channels[1:25]$N) / 25
 
 
 write.csv(df2, "~/Documents/GitHub/esade_fake_news/4_Politics/youtube_api_request_info/data/biden_labelling.csv")
