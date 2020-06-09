0335ecb40e536d4c28c0ea87413c548477ca4670
Rolf Stirnimann <rolf.stirnimann@alumni.esade.edu>
2020-06-09 12:02:19 +0200

    R Labelling

--- 4_Politics/python3_script/data/Labelling.R
+++ 4_Politics/python3_script/data/Labelling.R
@@ -1,58 +1,69 @@
 library(data.table)
-#install.packages('bit64')
-
-#library(sjmisc)
-#library(dplyr)
-
-#install.packages('devtools')
-#devtools::install_github("favstats/AllSideR")
-#allsides = AllSideR::allsides_data
 
 #load dataset
 df = fread("~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/20200504-193926_joe_biden.csv")
 
-#create relevant column equals 1 if it contains keywords
-#115 videos contain keywords in title or description columns
-df$relevant = ifelse(grepl('biden|trump|democrat|republican|politics|president', paste(df$title, df$description), ignore.case=TRUE), 1, 0)
-sum(df$relevant)
-
-#number of videos that contain keywords in title column
-#59 videos
-sum(ifelse(grepl('biden|trump|democrat|republican|politics|president', df$title, ignore.case=TRUE), 1, 0))
-
-#number of videos that contain keywords in description column
-#113
-sum(ifelse(grepl('biden|trump|democrat|republican|politics|president', df$description, ignore.case=TRUE), 1, 0))
-
-#Look for fake news
-df2 = df[relevant==1, .(title, description, channel)]
-
-#write csv with channels and genre
-channels = df[, .N, by='channel'][order(-N)]
-channels_genre = df[, .N, by=c('channel','genre')]
-write.csv(channels_genre, "~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/channels/20200504-193926_joe_biden_channels.csv", row.names=FALSE)
-
-#write csv with genre
-genre = df[, .N, by='genre'][order(-N)]
-write.csv(genre, "~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/genre/20200504-193926_joe_biden_genre.csv", row.names=FALSE)
-
 genre_relevant = fread("~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/genre/20200504-193926_joe_biden_genre.csv")
 
-df2 = merge(df[, c('title','description','genre','channel','id')], 
+#merge datasets to obtain relevant videos.
+df2 = merge(df, 
             genre_relevant, 
             by='genre')
 
-#percentage of videos that are relevant
+# 75% of videos are relevant
 mean(df2$relevant)
 
-#create table with only relevant videos
-relevant = df2[relevant == 1]
+#create new dataset with only relevant videos
+df_relevant = df2[relevant==1]
 
-#top25 channels weight
-sum(channels[1:25]$N) / sum(channels$N)
+df_relevant_channel = genre = df_relevant[, .N, by='channel'][order(channel)]
 
-#top25 channels - average number of videos per channel
-sum(channels[1:25]$N) / 25
+#export df_relevant and edit channels
+#write.csv(df_relevant_channel, "~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/channels/20200504-193926_joe_biden_channels_relevant.csv")
+
+#export df_relevant and edit channels
+#write.csv(allsides, "~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/channels/allsides_bias.csv")
+
+#load cleaned channel dataset
+df_updated = fread("~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/channels/20200504-193926_joe_biden_channels_relevant.csv", na.strings=c("","NA"), drop=c('Column1'))
+
+#load media bias dataset
+media_bias = fread("~/Documents/GitHub/esade_fake_news/4_Politics/python3_script/data/channels/media_bias_data.csv", drop='Website')
+
+#merge cleaned channel dataset with media bias
+df_merged = merge(df_updated,
+                  media_bias,
+                  by.x='media_bias',
+                  by.y='News Source')
+
+# 55% of all videos have a media bias
+sum(df_merged$N) / nrow(df)
+
+# 75% of relevant videos have a media bias 
+sum(df_merged$N) / nrow(df_relevant)
+
+#remove N column
+df_merged[, 'N':=NULL]
+
+#complete dataset
+df_merged2 = merge(df_relevant,
+                   df_merged,
+                   by='channel')
+
+#stats
+df_merged2[, .N, by=c('Bias','channel')][order(Bias, -N)]
+
+df_merged2[Bias=='Right-Center', .N, by='Bias']
+
+mytable = table(df_merged2$Bias)
+prop.table(mytable)
 
 
-write.csv(df2, "~/Documents/GitHub/esade_fake_news/4_Politics/youtube_api_request_info/data/biden_labelling.csv")
+tblFun <- function(x){
+  tbl <- table(x)
+  res <- cbind(tbl,round(prop.table(tbl)*100,2))
+  colnames(res) <- c('Count','Percentage')
+  res
+}
+
+do.call(rbind,lapply(df_merged2[, 'Bias'],tblFun))
