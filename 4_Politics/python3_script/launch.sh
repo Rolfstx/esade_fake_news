8779b8abf4cb02e3c02cb25ebba78688f69d1b35
Rolf Stirnimann <rstirne@gmail.com>
2020-06-21 23:13:08 +0200

    black lives matter youtube data

--- 4_Politics/python3_script/launch.sh
+++ 4_Politics/python3_script/launch.sh
@@ -1,1 +1,1 @@
-python3 youtube.py --query="George Floyd" --searches=1 --branch=8 --depth=6 --channels=True --key='AIzaSyBpIPMAow4TmfAAw7GaMwX2Bru_S8P2ndc'
+python3 youtube.py --query="black lives matter" --searches=1 --branch=7 --depth=6 --channels=True --key='AIzaSyBpIPMAow4TmfAAw7GaMwX2Bru_S8P2ndc'
